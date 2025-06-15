9bffcbf4421f951346dffceafc2af464606d579f
Ferisus app <tiwokwedok@gmail.com>
2025-06-15 19:55:09 +0700

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,1 +1,24 @@
+const radios = [
+  {
+    name: "Mumpuni FM",
+    url: "https://b.alhastream.com:5006/radio",
+    image: "https://cdn-icons-png.flaticon.com/512/727/727240.png"
+  },
+  {
+    name: "Sip FM",
+    url: "https://a3.alhastream.com:1275/sipfm",
+    image: "https://cdn-icons-png.flaticon.com/512/727/727245.png"
+  },
+  {
+    name: "Prambors FM",
+    url: "https://s1.cloudmu.id/listen/prambors_makassar/radio.mp3",
+    image: "https://cdn-icons-png.flaticon.com/512/727/727239.png"
+  }
+  // Tambahkan radio lainnya sesuai kebutuhan
+];
+
+const playerContainer = document.getElementById("playerContainer");
+const playerTitle = document.getElementById("playerTitle");
+const playButton = document.getElementById("playButton");
+const radi
 
