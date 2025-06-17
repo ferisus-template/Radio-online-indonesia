4a3bc1d88e30d0c6e1433e7013f720f71d15876a
Ferisus app <tiwokwedok@gmail.com>
2025-06-17 18:10:55 +0700

    Update Navbar.js

--- js/components/Navbar.js
+++ js/components/Navbar.js
@@ -3,23 +3,27 @@ export function Navbar() {
     <nav class="navbar">
       <div class="navbar-container">
         <div class="navbar-brand">🎵 FeriRadio</div>
-        <input 
-          type="text" 
-          id="radioSearchInput" 
-          placeholder="🔍 Cari radio..." 
-          class="radio-search"
-        />
+
+        <input
+          type="text"
+          id="radioSearchInput"
+          placeholder="🔍 Cari radio..."
+          class="radio-search"
+        />
+
         <button class="navbar-toggle" id="navbarToggle">☰</button>
-        <ul class="navbar-links-popup" id="navbarLinks">
+
+        <!-- Ganti class jadi navbar-links -->
+        <ul class="navbar-links" id="navbarLinks">
           <li><a href="#home">Home</a></li>
           <li><a href="#radio">Radio</a></li>
           <li><a href="#about">About</a></li>
         </ul>
-        
       </div>
     </nav>
   `;
 }
+
 
 
 // tambahkan event listener toggle setelah elemen dipasang ke DOM
