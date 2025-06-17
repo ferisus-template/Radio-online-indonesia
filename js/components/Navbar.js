a1e34bafebfdda2d7e0c0219540530272a4b45b1
Ferisus app <tiwokwedok@gmail.com>
2025-06-17 18:41:47 +0700

    Update Navbar.js

--- js/components/Navbar.js
+++ js/components/Navbar.js
@@ -13,11 +13,11 @@ export function Navbar() {
 
         <button class="navbar-toggle" id="navbarToggle">☰</button>
 
-        <!-- Ganti class jadi navbar-links -->
         <ul class="navbar-links-popup" id="navbarLinks">
           <li><a href="#home">Home</a></li>
           <li><a href="#radio">Radio</a></li>
           <li><a href="#about">About</a></li>
+        </ul>
         </ul>
       </div>
     </nav>
